6738779f68c333b4feeed5060564848116ed6c13
KevinKuntz255 <angelthebunny541@gmail.com>
2022-02-27 08:18:58 +0200

    gamer time

--- assets/preload/data/pip/script.lua
+++ assets/preload/data/pip/script.lua
@@ -1,31 +1,50 @@
-
 local xx = 520;
 local yy = 580;
 local xx2 = 820;
 local yy2 = 580;
 local ofs = 20;
 local followchars = true;
-local del = 0;
-local del2 = 0;
 local allowCountdown = false
+
+local usedSpace = false
+
+function onCreate()
+	addCharacterToList('violet', 'dad');
+	
+	makeAnimatedLuaSprite('violet', 'characters/Definitive_Violet_Assets', getProperty('dad2.x'), getProperty('dad2.y'));
+	addLuaSprite('violet', false)
+	setObjectOrder('violet', getObjectOrder('gfGroup'))
+	setProperty('violet.visible', false)
+end
 
 function onStartCountdown()
 	if not allowCountdown and isStoryMode and not seenCutscene then
-		startVideo('intro');
+		startVideo('shoot');
 		allowCountdown = true;
 		return Function_Stop;
 	end
 	return Function_Continue;
 end
-
-
-function onUpdate()
-	if del > 0 then
-		del = del - 1
+function onStepHit()
+	if curStep == 510 or curStep == 511 then
+		setProperty('violet.visible', true)
+		setProperty('dad2.visible', false)
+		setProperty('dad2.alpha', 0)
 	end
-	if del2 > 0 then
-		del2 = del2 - 1
-	end
+end
+
+function onGameOverStart()
+	setProperty('camFollow.y', getProperty('camFollow.y') - 300)
+end
+
+function onUpdate(elapsed)
+	setProperty('violet.x', getProperty('dad2.x'))
+	setProperty('violet.y', getProperty('dad2.y') - 50)
+	setProperty('violet.scale.x', getProperty('dad2.scale.x'))
+	setProperty('violet.scale.y', getProperty('dad2.scale.y'))
+	setProperty('violet.offset.x', getProperty('dad2.offset.x'))
+	setProperty('violet.offset.y', getProperty('dad2.offset.y'))
+	setProperty('violet.animation.frameIndex', getProperty('dad2.animation.frameIndex'))
     if followchars == true then
         if mustHitSection == false then
             if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
@@ -59,7 +78,6 @@ function onUpdate()
                 triggerEvent('Camera Follow Pos',xx,yy)
             end
         else
-
             if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
                 triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
             end
@@ -79,5 +97,4 @@ function onUpdate()
     else
         triggerEvent('Camera Follow Pos','','')
     end
-    
 end
