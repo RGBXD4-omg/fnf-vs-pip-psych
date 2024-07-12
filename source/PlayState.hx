c72beed5d1bc2a5dbb8c19304dc7fe72943d5f33
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-12 09:18:41 +0800

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1233,11 +1233,15 @@ class PlayState extends MusicBeatState
 		#end
 
 
-		FlxG.stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyPress);
-		FlxG.stage.addEventListener(KeyboardEvent.KEY_UP, onKeyRelease);
+		if (!ClientPrefs.controllerMode)
+		{
+			FlxG.stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyPress);
+			FlxG.stage.addEventListener(KeyboardEvent.KEY_UP, onKeyRelease);
+		}
 		
 
 		Conductor.safeZoneOffset = (ClientPrefs.safeFrames / 60) * 1000;
+		
 		callOnLuas('onCreatePost', []);
 		
 		super.create();
@@ -4339,7 +4343,7 @@ public function startVideo(name:String)
 			var key:Int = getKeyFromEvent(eventKey);
 			//trace('Pressed: ' + eventKey);
 	
-			if (!cpuControlled && !paused && key > -1 && FlxG.keys.checkStatus(eventKey, JUST_PRESSED))
+			if (!cpuControlled && !paused && key > -1 && FlxG.keys.checkStatus(eventKey, JUST_PRESSED || ClientPrefs.controllerMode))
 			{
 				if(!boyfriend.stunned && generatedMusic && !endingSong)
 				{
@@ -4476,6 +4480,25 @@ public function startVideo(name:String)
 
 	private function keyShit():Void
 		{
+		
+		// TO DO: Find a better way to handle controller inputs, this should work for now
+		if (ClientPrefs.controllerMode)
+		{
+			var controlArray:Array<Bool> = [
+				controls.NOTE_LEFT_P,
+				controls.NOTE_DOWN_P,
+				controls.NOTE_UP_P,
+				controls.NOTE_RIGHT_P
+			];
+			if (controlArray.contains(true))
+			{
+				for (i in 0...controlArray.length)
+				{
+					if (controlArray[i])
+						onKeyPress(new KeyboardEvent(KeyboardEvent.KEY_DOWN, true, true, -1, keysArray[i][0]));
+				}
+			}
+		}
 			// FlxG.watch.addQuick('asdfa', upP);
 			if (!boyfriend.stunned && generatedMusic)
 			{
@@ -4500,8 +4523,26 @@ public function startVideo(name:String)
 				} else if (boyfriend.holdTimer > Conductor.stepCrochet * 0.001 * boyfriend.singDuration && boyfriend.animation.curAnim.name.startsWith('sing')
 				&& !boyfriend.animation.curAnim.name.endsWith('miss') && !boyfriend.animation.curAnim.name.contains('dodge'))
 					boyfriend.dance();
+					
+					// TO DO: Find a better way to handle controller inputs, this should work for now
+		if (ClientPrefs.controllerMode)
+		{
+			var controlArray:Array<Bool> = [
+				controls.NOTE_LEFT_R,
+				controls.NOTE_DOWN_R,
+				controls.NOTE_UP_R,
+				controls.NOTE_RIGHT_R
+			];
+			if (controlArray.contains(true))
+			{
+				for (i in 0...controlArray.length)
+				{
+					if (controlArray[i])
+						onKeyRelease(new KeyboardEvent(KeyboardEvent.KEY_UP, true, true, -1, keysArray[i][0]));
+				}
 			}
 		}
+	}
 
 	function noteMiss(daNote:Note):Void { //You didn't hit the key and let it go offscreen, also used by Hurt Notes
 		//Dupe note remove
@@ -4996,8 +5037,11 @@ public function startVideo(name:String)
 		luaArray = [];
 
 	
-		FlxG.stage.removeEventListener(KeyboardEvent.KEY_DOWN, onKeyPress);
-		FlxG.stage.removeEventListener(KeyboardEvent.KEY_UP, onKeyRelease);
+		if (!ClientPrefs.controllerMode)
+		{
+			FlxG.stage.removeEventListener(KeyboardEvent.KEY_DOWN, onKeyPress);
+			FlxG.stage.removeEventListener(KeyboardEvent.KEY_UP, onKeyRelease);
+		}
 		
 
 		super.destroy();
