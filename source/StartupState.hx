6600b0a645dc2ec178b58d9b972771b2ce64074f
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-13 14:27:01 +0800

    Update StartupState.hx

--- source/StartupState.hx
+++ source/StartupState.hx
@@ -17,6 +17,7 @@ class StartupState extends FlxState
     public var camHUD:FlxCamera;
     var bg:FlxSprite;
 	var skipText:FlxText;
+	public var inCutscene:Bool = false;
 
     override public function create():Void
     {	
@@ -29,49 +30,37 @@ class StartupState extends FlxState
 		FlxG.cameras.add(camHUD);
 
 
-    function startDaVideo(name:String):Void {
-            #if VIDEOS_ALLOWED
-            var foundFile:Bool = true;
-               var fileName = Paths.video(name);
-	    
-                #if sys
-                if(FileSystem.exists(fileName)){
-                #else
-                if(OpenFlAssets.exists(fileName)){
-                #end
+    public function startVideo(name:String)
+	{
+		#if VIDEOS_ALLOWED
+		inCutscene = true;
+
+		var filepath:String = Paths.video(name);
+		#if sys
+		if (!FileSystem.exists(filepath))
+		#else
+		if (!OpenFlAssets.exists(filepath))
+		#end
+		{
+			FlxG.log.warn('Couldnt find video file: ' + name);
+			return;
 		}
-			
-                bg = new FlxSprite(-FlxG.width, -FlxG.height).makeGraphic(FlxG.width * 3, FlxG.height * 3, FlxColor.BLACK);
-                bg.scrollFactor.set();
-                bg.cameras = [camHUD];
-                add(bg);
 
-                skipText = new FlxText(0, 0, FlxG.width, "PRESS SPACE TO SKIP", 20);
-                skipText.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-                skipText.scrollFactor.set();
-                skipText.borderSize = 1.25;
-                skipText.y = FlxG.height * 0.89 + 36;
-                skipText.visible = false;
-
-		var doge:VideoHandler = new VideoHandler();
-    
-                doge.finishCallback = function() {
-                    remove(bg);
-                    remove(skipText);
-                    FlxG.switchState(new TitleState());
-                }
-                add(skipText);
-
-                return;
-
-            #else
-                FlxG.log.warn('Couldnt find video file: ' + fileName);
-                FlxG.switchState(new TitleState());
-
-            #end
-            FlxG.switchState(new TitleState());
-        }
-        startDaVideo("Newgrounds");
+		var video:VideoHandler = new VideoHandler();
+		video.playVideo(filepath);
+		video.finishCallback = function()
+		{
+			FlxG.switchState(new TitleState());
+			return;
+		}
+		#else
+		FlxG.log.warn('Platform not supported!');
+		FlxG.switchState(new TitleState());
+		return;
+		#end
+	}
+	    
+        startVideo("Newgrounds");
 
         super.create();
     }
