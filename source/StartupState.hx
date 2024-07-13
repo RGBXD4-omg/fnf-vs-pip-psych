860b9d0de8bfb9047d7a5b743be57bc0261a6af7
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-13 16:35:57 +0800

    Update StartupState.hx

--- source/StartupState.hx
+++ source/StartupState.hx
@@ -8,15 +8,20 @@ import sys.FileSystem;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.FlxG;
+#if VIDEOS_ALLOWED
+import VideoHandler;
+#end
 
 class StartupState extends FlxState
 {
     public var camHUD:FlxCamera;
     var bg:FlxSprite;
-	var skipText:FlxText;
+    var skipText:FlxText;
+    public var inCutscene:Bool = false;
 
     override public function create():Void
-    {	Paths.clearStoredMemory();
+    {	
+	        Paths.clearStoredMemory();
 		Paths.clearUnusedMemory();
 
 		camHUD = new FlxCamera();
@@ -25,60 +30,44 @@ class StartupState extends FlxState
 		FlxG.cameras.add(camHUD);
 
 
-    function startDaVideo(name:String):Void {
-            #if VIDEOS_ALLOWED
-            var foundFile:Bool = false;
-            var fileName:String = "Newgrounds";
-            #if sys
-            if(FileSystem.exists(fileName)) {
-                foundFile = true;
-            }
-            #end
-    
-            if(!foundFile) {
-                fileName = Paths._video(name);
-                #if sys
-                if(FileSystem.exists(fileName)) {
-                #else
-                if(OpenFlAssets.exists(fileName)) {
-                #end
-                    foundFile = true;
-                }
-            }
-    
-            if(foundFile) {
-                bg = new FlxSprite(-FlxG.width, -FlxG.height).makeGraphic(FlxG.width * 3, FlxG.height * 3, FlxColor.BLACK);
-                bg.scrollFactor.set();
-                bg.cameras = [camHUD];
-                add(bg);
-
+    public function startVideo(name:String)
+	{
+		#if VIDEOS_ALLOWED
+		inCutscene = true;
+		
+		#if sys
+		if (!FileSystem.exists(filepath))
+		#else
+		if (!OpenFlAssets.exists(filepath))
+		#end
+		{
+			FlxG.log.warn('Couldnt find video file: ' + name);
+			FlxG.switchState(new TitleState());
+			return;
+		}
+		
                 skipText = new FlxText(0, 0, FlxG.width, "PRESS SPACE TO SKIP", 20);
                 skipText.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
                 skipText.scrollFactor.set();
                 skipText.borderSize = 1.25;
                 skipText.y = FlxG.height * 0.89 + 36;
                 skipText.visible = false;
-    
-                (new FlxVideo(fileName)).finishCallback = function() {
-                    remove(bg);
-                    remove(skipText);
-                    FlxG.switchState(new TitleState());
-                }
                 add(skipText);
 
-                return;
-
-            }
-            else
-            {
-                FlxG.log.warn('Couldnt find video file: ' + fileName);
-                FlxG.switchState(new TitleState());
-
-            }
-            #end
-            FlxG.switchState(new TitleState());
-        }
-        startDaVideo('Newgrounds');
+		var video:VideoHandler = new VideoHandler();
+		video.playVideo(Paths.video('Newgrounds');
+		video.finishCallback = function()
+		{
+			FlxG.switchState(new TitleState());
+			remove(skipText);
+			return;
+		}
+		#else
+		FlxG.log.warn('Platform not supported!');
+		FlxG.switchState(new TitleState());
+		return;
+		#end
+	}
 
         super.create();
     }
@@ -88,5 +77,5 @@ class StartupState extends FlxState
     override public function update(elapsed:Float):Void
      {
         super.update(elapsed);
-    }
-  }
+     }
+}
